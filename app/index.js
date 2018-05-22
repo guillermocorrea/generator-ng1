6f9c2cf9ad644f0cc9390f00bf30d7346f1374be
Guillermo Correa <guillermo.correa.99@@gmail.com>
2018-05-22 16:06:08 -0500

    Changed root path

--- app/index.js
+++ app/index.js
@@ -24,7 +24,7 @@ module.exports = class extends Generator {
             const className = upperCamelCase(answers.name);
             const htmlTemplateName = answers.name + '.component.html';
             const componentName = className + 'Component';
-            const appRoot = 'app/src/';
+            const appRoot = 'src/app/';
             // create destination folder
             this.destinationRoot(answers.name);
             this.fs.copyTpl(
